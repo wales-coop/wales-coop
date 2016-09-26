a4eef16584a6853ebfb9ca877ea5f6176c355477
Shadows666 <lifeloveregretsxe@gmail.com>
2016-09-26 18:43:53 +0100

    Set up links for data and seperate files for sections

---
+++ public/content-data-financing.js
@@ -0,0 +1,30 @@
+const contentDataFinancing = [
+  {
+    title: 'Sources of Finance',
+    subtitle: '',
+    topic: 'Financing a social business',
+    URL: 'https://businesswales.gov.wales/socialbusinesswales/sources-finance',
+    resources: [
+    ],
+  },
+  {
+    title: 'Lenders',
+    subtitle: 'The following are links to suitable lenders that are familiar in dealing with Social Businesses. However, with a strong business plan and a strong team, do not rule out the possibility of approaching traditional high street banks.',
+    topic: 'Financing a social business',
+    URL: 'https://businesswales.gov.wales/socialbusinesswales/lenders',
+    resources: [
+    ],
+  },
+  {
+    title: 'Books and Online Resources',
+    subtitle: '',
+    topic: 'Financing a social business',
+    URL: 'https://businesswales.gov.wales/socialbusinesswales/books-and-online-resources',
+    resources: [
+      {
+        title: 'Co-operative Loan Agreement Template',
+        URL: 'https://businesswales.gov.wales/socialbusinesswales/sites/socialbusiness/files/Co-operative%20Loan%20Agreement%20template%20v4_0.docx',
+      },
+    ],
+  },
+];
---
+++ public/content-data-running-governance.js
@@ -0,0 +1,78 @@
+const contentDataRunningGivernance = [
+  {
+    title: 'Social Business Governance Zone',
+    subtitle: 'Social Businesses are at the forefront of the development of good governance, combining as they do the requirements to provide the governance functions of commercial businesses and organisations providing benefit to Members or some community.',
+    topic: 'Running a social business - Social Business Governance Zone',
+    URL: 'https://businesswales.gov.wales/socialbusinesswales/social-business-governance-zone',
+    resources: [
+      {
+        title: 'Download a Governance Questionnaire',
+        URL: 'https://businesswales.gov.wales/socialbusinesswales/sites/socialbusiness/files/Governance%20Questionnaire%20v4_0.docx',
+      },
+    ],
+  },
+  {
+    title: 'Developing required Policies',
+    subtitle: '',
+    topic: 'Running a social business - Social Business Governance Zone',
+    URL: 'https://businesswales.gov.wales/socialbusinesswales/developing-required-policies',
+    resources: [
+      {
+        title: 'Policy Checklist',
+        URL: 'https://businesswales.gov.wales/socialbusinesswales/sites/socialbusiness/files/Policy_Checklist_v5ac.xls',
+      },
+      {
+        title: 'Guide to Employing People 2015',
+        URL: 'https://businesswales.gov.wales/socialbusinesswales/sites/socialbusiness/files/Guide%20to%20Employing%20People%20-%202015%20FINAL.pdf',
+      },
+    ],
+  },
+  {
+    title: 'Meeting Legal Reporting Requirements',
+    subtitle: 'A Social Business will be established as some kind of legal entity. A legal entity has important rights, most importantly to enter into contracts with suppliers, customers, employees, project partners and the like.',
+    topic: 'Running a social business - Social Business Governance Zone',
+    URL: 'https://businesswales.gov.wales/socialbusinesswales/meeting-legal-reporting-requirements',
+    resources: [
+      {
+        title: 'Download a Guide to Business Compliance for Social Businesses',
+        URL: 'https://businesswales.gov.wales/socialbusinesswales/sites/socialbusiness/files/Business%20Compliance%20vSBW_0.pdf',
+      },
+    ],
+  },
+  {
+    title: 'Stakeholder and Membership Engagement',
+    subtitle: 'One of the great strengths of Social Businesses is that they engage in ownership and influence wider ranges and greater numbers of Stakeholders and Members than most businesses of similar size.',
+    topic: 'Running a social business - Social Business Governance Zone',
+    URL: 'https://businesswales.gov.wales/socialbusinesswales/stakeholder-and-membership-engagement',
+    resources: [
+    ],
+  },
+  {
+    title: 'Providing Control and Oversight',
+    subtitle: 'A Social Business, as a legal entity, is a body corporate and must obey all laws, not only the Companies Act or Co-operative Societies Act (or equivalent legislation for the precise legal form taken by the Social Business) but also those that relate to Health and Safety, employment, tax, trade, contracts, finance, equal opportunity, protection of the vulnerable and much, much more.',
+    topic: 'Running a social business - Social Business Governance Zone',
+    URL: 'https://businesswales.gov.wales/socialbusinesswales/providing-control-and-oversight',
+    resources: [
+    ],
+  },
+  {
+    title: 'Developing Governance Capacity',
+    subtitle: 'A fundamental component of success for a Social Business is the strength of its Governance. Governance is the systems and processes concerned with ensuring the overall direction, supervision and accountability of an organisation.',
+    topic: 'Running a social business - Social Business Governance Zone',
+    URL: 'https://businesswales.gov.wales/socialbusinesswales/developing-governance-capacity',
+    resources: [
+      {
+        title: 'Download a Governance Questionnaire',
+        URL: 'https://businesswales.gov.wales/socialbusinesswales/sites/socialbusiness/files/Governance%20Questionnaire%20v4_0_0.docx',
+      },
+    ],
+  },
+  {
+    title: 'Social Accounting Zone',
+    subtitle: 'We like to believe that Social Businesses pursuing social aims are more deserving of support from funding bodies, local communities and customers than enterprises for which the primary function is financial return for those that own and finance them.',
+    topic: 'Running a social business',
+    URL: 'https://businesswales.gov.wales/socialbusinesswales/social-accounting-zone',
+    resources: [
+    ],
+  },
+];
