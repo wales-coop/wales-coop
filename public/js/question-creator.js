b77246472dde0942a960626c5f68292a7bdb0e2f
mcelearr <rorymcelearney@gmail.com>
2016-09-28 18:35:49 +0100

    add question creator

---
+++ public/js/question-creator.js
@@ -0,0 +1,17 @@
+(function() {
+  var questionForm = $('.question-form');
+  questions.map(function(questionObject) {
+    console.log(questionObject);
+  })
+  // topics.map(function(topic, index) {
+  //   var topicDiv = document.createElement('div');
+  //   topicDiv.className = 'topic';
+  //   topicDiv.setAttribute('style', 'display: none');
+  //   topicDiv.id = topic;
+  //   var headerElem = document.createElement('h2');
+  //   var headerText = document.createTextNode(topic);
+  //   headerElem.appendChild(headerText);
+  //   topicDiv.appendChild(headerElem);
+  //   contentHolder.appendChild(topicDiv);
+  // })
+})()
