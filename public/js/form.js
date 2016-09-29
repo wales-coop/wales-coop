1cbe79de73a8aff2fe36f7acfc4ff4267f4f5cf4
mcelearr <rorymcelearney@gmail.com>
2016-09-29 14:17:15 +0100

    almost there

--- public/js/form.js
+++ public/js/form.js
@@ -1,26 +1,51 @@
 (function() {
 
-function CookieQuestionCount() {
+function cookieQuestionCount() {
   var completedQuestions = docCookies.keys();
   return completedQuestions.length;
 }
 
-var questionCount = CookieQuestionCount() > 0 ? CookieQuestionCount() : 0;
+var questionCount = cookieQuestionCount() > 0 ? cookieQuestionCount() : 0;
 
 function incrementSessionQuestionCount() {
     questionCount += 1;
 }
 
-function setCookie(answer, questionNo) {
-  // check to see if cookie already exists and delete if it does
-  docCookies.setItem(questions[questionNo - 2].question, answer);
+// add check for cookies here
+var answerArray = questions.map(function(questionObject) {
+  questionObject.answer = false;
+  return questionObject;
+})
+
+function updateAnswers(answer, questionNo) {
+  answerArray[questionNo - 2].answer = answer;
 }
 
-function updateDoughnut(answer, questionNo) {
-  config.data.labels.push('data #' + config.data.labels.length);
-  config.data.datasets[0].data.push(20);
-  config.data.datasets[0].backgroundColor.push();
-  window.myDoughnut.update();
+function updateDoughnut(questionNo) {
+  var topicPercentages = topics.map(function(topic) {
+    var topicAnswers = answerArray.filter(function(answerObject) {
+      return answerObject.topic === topic;
+    });
+    var trueTopicAnswers = topicAnswers.filter(function(answerObject) {
+      return answerObject.answer === true;
+    });
+    var percentageTrueAnswers = trueTopicAnswers.length / topicAnswers.length;
+    var percentageOfDoughnut = percentageTrueAnswers * 100 / 5;
+    var rObj = {};
+    rObj[topic] = percentageOfDoughnut;
+    return rObj;
+  }).slice(0,5);
+  var currentTopicPercentage = topicPercentages.filter(function(topicObject) {
+    return Object.keys(topicObject)[0] === questions[questionNo - 2].topic;
+  })
+  var valueOfCurrentTopicPercentage = currentTopicPercentage[0][Object.keys(currentTopicPercentage[0]])
+  console.log(valueOfCurrentTopicPercentage);
+  if (questions[questionNo - 2].topic !== questions[questionNo - 1].topic) {
+    config.data.labels.push('data #' + config.data.labels.length);
+    config.data.datasets[0].data.push(20);
+    config.data.datasets[0].backgroundColor.push();
+    window.myDoughnut.update();
+  }
 }
 
 function changeTopic(questionNo) {
@@ -49,6 +74,11 @@ function changeTopic(questionNo) {
   }
 }
 
+function setCookie(answer, questionNo) {
+  // check to see if cookie already exists and delete if it does
+  docCookies.setItem(questions[questionNo - 2].question, answer);
+}
+
 function changeQuestion() {
   $.each($('.question-title'), function(index, question){
     if ($(question).is(':visible')) {
@@ -62,7 +92,8 @@ function changeQuestion() {
 $('.question-button').click(function(event) {
   incrementSessionQuestionCount();
   var answer = event.target.id === 'question-button-yes'? true : false;
-  updateDoughnut(answer, questionCount);
+  updateAnswers(answer, questionCount);
+  updateDoughnut(questionCount);
   changeTopic(questionCount);
   setCookie(answer, questionCount);
   changeQuestion();
