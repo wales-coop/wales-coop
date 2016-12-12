562346fdcc3c4c83e7c0272ed6e031c5ba53b0a9
samgalson@gmail.com <samgalson@gmail.com>
2016-12-12 14:53:15 +0000

    chore(deployment): revert to using validator.setDefaults

--- src/client/register-form.js
+++ src/client/register-form.js
@@ -4,6 +4,17 @@ import 'jquery-validation';
 export default function () {
   $('select').material_select();
 
+  $.validator.setDefaults({
+    errorClass: 'invalid',
+    validClass: 'valid',
+    errorPlacement(error, element) {
+      $(element)
+        .closest('form')
+        .find(`label[for='${element.attr('id')}']`)
+        .attr('data-error', error.text());
+    },
+  });
+
   $('#register-form').validate({
     rules: {
       confirmPassword: {
@@ -14,14 +25,6 @@ export default function () {
       confirmPassword: {
         equalTo: 'Passwords must match',
       },
-    },
-    errorClass: 'invalid',
-    validClass: 'valid',
-    errorPlacement(error, element) {
-      $(element)
-        .closest('form')
-        .find(`label[for='${element.attr('id')}']`)
-        .attr('data-error', error.text());
     },
   });
   return undefined;
