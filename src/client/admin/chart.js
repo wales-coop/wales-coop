238eae92051b742cfbfcf62bbf1ef81af7470f2d
mattiaS <mattia@3doers.it>
2016-12-15 15:48:56 +0000

    chore(data): remove dummy data

--- src/client/admin/chart.js
+++ src/client/admin/chart.js
@@ -78,11 +78,4 @@ export default (data) => {
         .style('visibility', 'hidden'),
        );
 };
-/* let data = [*/
-    // { id: 'q1', frequency: 79, text: 'Have you started your social enterprise?' },
-    // { id: 'q2', frequency: 100, text: 'Do you have an engaged group of people?' },
-    // { id: 'q3', frequency: 200, text: 'Have you formed a legal structure?' },
-    // { id: 'q5', frequency: 125, text: 'Are you bidding for contracts or seeking funding?' },
-    // { id: 'q7', frequency: 400, text: 'Are you employing staff?' },
-    // { id: 'q9', frequency: 300, text: 'Are you confident in your organisations governance?' },
-  /* ];*/
+
